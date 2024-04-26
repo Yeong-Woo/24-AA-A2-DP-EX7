a44a95d5857b62e4bab3f99be5df80311a3b2b73
yw0.kim <dn9078@naver.com>
2024-04-26 10:49:29 +0900

    Init

---
+++ maze/snow/SnowWhiteMazeFactory.java
@@ -0,0 +1,24 @@
+
+package maze.snow; 
+
+import maze.*;
+
+// don't forget to inherit
+public class SnowWhiteMazeFactory  { 
+  
+  // create the Wall
+  public Wall makeWall() { 
+	  return null;	  	  
+  }
+
+  // create the Room
+  public Room makeRoom(int roomNumber) { 
+	  return null;	  	  
+  }
+
+  // create the Door
+  public Door makeDoor(Room room1, Room room2) { 
+	  return null;
+	  	  
+  }
+}
---
+++ maze/snow/SnowWhiteMazeGameCreator.java
@@ -0,0 +1,22 @@
+
+package maze.snow; 
+
+import maze.*;
+
+//don't forget to inherit
+public class SnowWhiteMazeGameCreator  { 
+
+	public Wall makeWall() { 
+		  return null;
+		  	  
+	  }
+
+	  public Room makeRoom(int roomNumber) { 
+		  return null;
+		  	  
+	  }
+
+	  public Door makeDoor(Room room1, Room room2) { 
+		  return null;
+	  }
+}
